f2fe360ef4f54a473060a4713a460020d93c6c97
saadhmed5 <s4096321@student.rmit.edu.au>
2026-05-17 14:24:37 +1000

    Changes

--- trashcan/app/page.tsx
+++ trashcan/app/page.tsx
@@ -182,7 +182,7 @@ export default function Home(): JSX.Element {
 
       {/* ================= FOOTER ================= */}
       <footer className="bg-green-700 text-white text-center p-4">
-        Rubbish bin Collection company 2026
+        COSC2758 Final Programming Challenge © 2026
       </footer>
     </div>
   );
